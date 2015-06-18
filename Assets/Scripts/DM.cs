840ac60ef8346ffc67710e3d160838b72a0c1ced
sinodaur <cperric@gmail.com>
2015-06-18 18:15:43 -0400

    namespaces, button control, gizmo lines
    -Added namespaces to scripts to folders reflect that.
    -modified PlayerControl script to prevent action button from
    continuously being read while depressed.
    -Added some lines connecting the current two system objects to
    eventually give a hierarchy view of persistent system objects and their
    relation to one another.

--- Assets/Scripts/DM.cs
+++ Assets/Scripts/DM.cs
@@ -1,61 +1,76 @@
 ﻿using UnityEngine;
 using System.Collections;
+using MyGame.DataObjects;
+using MyGame.PlayerInterface;
+using MyGame.Static;
 
 //This is the script for the DM of the game
 
-public class DM : MonoBehaviour {
-	
-	GameObject player;
-	
-	PlayerController playerController;
-	
-	
-	// This class gives the DM GameObject central control over the game and all it's objects.
-	void Start () 
-	{
-		// find the main player of the game
-		player = GameObject.Find("Player");
+namespace MyGame 
+{
+
+	public class DM : MonoBehaviour {
 		
-		// get ahold of the PlayerController instance that gives control of GameObjects to the player
-		playerController = GameObject.Find("PlayerController").GetComponent<PlayerController>();
+		GameObject player;
+		GameObject playerControllerGO;
+		PlayerController playerController;
 		
-		// tell the PlayerController to give control to the Player GameObject
-		playerController.setGameObject(player);
-		playerController.startObjectPlayerControl();	
 		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+		// This class gives the DM GameObject central control over the game and all it's objects.
+		void Start () 
+		{
+			// find the main player of the game
+			player = GameObject.Find("Player");
+			
+			// get ahold of the PlayerController instance that gives control of GameObjects to the player
+			playerController = GameObject.Find("PlayerController").GetComponent<PlayerController>();
+			
+			// tell the PlayerController to give control to the Player GameObject
+			playerController.setGameObject(player);
+			playerController.startObjectPlayerControl();	
+			
+		}
 		
-	}
-	
-	
-	// fuction to notify DM of player pushing a non-movement button
-	public void notifyPlayerRequest(string button)
-	{
-		// suspend player control until checked by DM
-		playerController.stopObjectPlayerControl();
-		// action key that was pressed
-		if (button == "Fire1") seeWhatPlayerWants();
+		// Update is called once per frame
+		void Update () {
+			
+		}
 		
-		// give control back to player
-		playerController.startObjectPlayerControl();
-	}
-	
-	void seeWhatPlayerWants() 
-	{
-		// get the GameObject that PlayerController is currently giving control to player
-		player = playerController.getGameObject();
 		
-		// get the touch information 
-		ObjectTouch objectTouchedByPlayer = ObjectDetection.whatTouching(player);
-		string side = objectTouchedByPlayer.getSideAffected();
-		GameObject go = objectTouchedByPlayer.getTheGameObject();
+		// fuction to notify DM of player pushing a non-movement button
+		public void notifyPlayerRequest(string button)
+		{
+			// suspend player control until checked by DM
+			playerController.stopObjectPlayerControl();
+			// action key that was pressed
+			if (button == "Fire1") seeWhatPlayerWants();
+			
+			// give control back to player
+			playerController.startObjectPlayerControl();
+		}
 		
-		if (objectTouchedByPlayer.getTheGameObject() != null)
-			Debug.Log("The " + side + " of the " + go + " game object is being touched" );
-		else
-			Debug.Log("Nothing is being touched" );
+		void seeWhatPlayerWants() 
+		{
+			// get the GameObject that PlayerController is currently giving control to player
+			player = playerController.getGameObject();
+			
+			// get the touch information 
+			ObjectTouch objectTouchedByPlayer = ObjectDetection.whatTouching(player);
+			string side = objectTouchedByPlayer.getSideAffected();
+			GameObject go = objectTouchedByPlayer.getTheGameObject();
+			
+			if (objectTouchedByPlayer.getTheGameObject() != null)
+				Debug.Log("The " + side + " of the " + go + " game object is being touched" );
+			else
+				Debug.Log("Nothing is being touched" );
+		}
+		
+		void OnDrawGizmos()
+		{
+			playerControllerGO = GameObject.Find("PlayerController");
+			Gizmos.color = Color.yellow;
+			Gizmos.DrawLine(transform.position, playerControllerGO.transform.position);
+		}
+		
 	}
 }
--- Assets/Scripts/Data Objects/ObjectTouch.cs
+++ Assets/Scripts/Data Objects/ObjectTouch.cs
@@ -1,35 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ObjectTouch
-{
 
-	GameObject theGameObject;
-	string sideAffected;
+
+namespace MyGame.DataObjects
+{
 	
 	
-	public ObjectTouch()
+	
+	public class ObjectTouch
 	{
-			
-	}
 	
-	public ObjectTouch(GameObject theGO, string side)
-	{
-		theGameObject = theGO;
-		sideAffected = side;	
-	}
-	
-	public string getSideAffected()
-	{
+		GameObject theGameObject;
+		string sideAffected;
 		
-		return sideAffected;
+		
+		public ObjectTouch()
+		{
+				
+		}
+		
+		public ObjectTouch(GameObject theGO, string side)
+		{
+			theGameObject = theGO;
+			sideAffected = side;	
+		}
+		
+		public string getSideAffected()
+		{
+			
+			return sideAffected;
+		}
+		
+		public GameObject getTheGameObject()
+		{
+			return theGameObject;
+		}
+		
+		
+		
 	}
-	
-	public GameObject getTheGameObject()
-	{
-		return theGameObject;
-	}
-	
-	
-	
 }
