4aa1e5b652cebbb5df47c6ae75f5c888c2badb3d
sinodaur <cperric@gmail.com>
2015-07-04 15:48:00 -0400

    Added a PlayerCamera class
    Added a PlayerCamera class to allow the PlayerCamera object to track the
    player.

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour {
+
+	
+	Camera theCamera;
+	GameObject thePlayer;
+	Vector3 playerPos; 
+	
+	
+	// Use this for initialization
+	void Start () {
+		theCamera = this.GetComponent("Camera") as Camera;
+		thePlayer = GameObject.Find("Player");
+		playerPos = new Vector3();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		playerPos = thePlayer.transform.position;
+		playerPos.Set(playerPos.x, playerPos.y + 19f, playerPos.z - 20f);
+		transform.position = playerPos;		
+	}
+}
